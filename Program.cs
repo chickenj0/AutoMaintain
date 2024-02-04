9eed1cdb46a3b079aeb2338677fba53efbd2b8f0
H <H@HENRY2U>
2024-02-04 12:45:45 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+Console.WriteLine("Hello, World!");
+
+# Check for Windows updates
+$updates = Get - WindowsUpdate
+
+# Check if updates are available
+if ($updates.Count - gt 0) {
+# Install updates
+    Install - WindowsUpdate - MicrosoftUpdate - AcceptAll - Force
+
+    # Check if the update was successful
+    if ($?) {
+        "Windows 10/11 Updated" | Out - File - FilePath "$env:userprofile\Desktop\update_status.txt" - Force
+        Start - Process wordpad - ArgumentList "$env:userprofile\Desktop\update_status.txt"
+    } else
+    {
+        "Update Failed" | Out - File - FilePath "$env:userprofile\Desktop\update_status.txt" - Force
+        Start - Process wordpad - ArgumentList "$env:userprofile\Desktop\update_status.txt"
+    }
+} else
+{
+    "No updates available" | Out - File - FilePath "$env:userprofile\Desktop\update_status.txt" - Force
+    Start - Process wordpad - ArgumentList "$env:userprofile\Desktop\update_status.txt"
+}
