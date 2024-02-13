f1e70b35de00896a17025089bb39592f1b2ee587
chickenj0 <121471036+chickenj0@users.noreply.github.com>
2024-02-13 14:01:44 +0800

    put in documentation to copy paste into script

--- PsGalleryScript-HENRY2U.ps1
+++ PsGalleryScript-HENRY2U.ps1
@@ -2,30 +2,43 @@
 # install powershell
 winget install --id Microsoft.Powershell --source winget
 
+Install PowerShell
+
+
 # set execution policy PS
 Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned 
 
+Permissions set - RemoteSigned 
+
+
 # Create restore checkpoint
 Enable-ComputerRestore -Drive "C:\" && Checkpoint-Computer -Description "MaintenanceRestorePoint" -RestorePointType "MODIFY_SETTINGS"  
+
+Enable Restore Point & Restore point 'MaintenanceRestorePoint' created
+
 
 # install powershellget
 Find-Module -Name PowerShellGet | Install-Module  
 Install-PackageProvider -Name NuGet
 
 # install relevant modules
-Install-Module -Name PSWindowsUpdate PowerShellGet
-Install-Module -Name PowerShellGet
+Install-Module PSWindowsUpdate && install-module PowerShellGet
 
+Install PowerShellGet & PSWindowsUpdate
 
 # update WindowsOS & WinGet
 get-windowsupdate -install && winget upgrade --all
+
+PSWindowsUpdate update - ()
+WinGet upgrade - ()
 
 # update windows Office and 365
 cmd.exe
 whoami
 "C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe" /update <user>
 
-Install-Module -Name DellBIOSProvider
+Office & 365 updates - OfficeC2RClient.exe - checked
+
 
 # disk cleanup 
 cleanmgr /sagerun:1 -Verbose 
@@ -43,8 +56,23 @@ Start-Process -FilePath "C:\Windows\System32\sfc.exe" -ArgumentList 'verifyonly'
 Start-Process -FilePath "C:\Windows\System32\sfc.exe" -ArgumentList 'scannow'
 
 
+BitDefender Endpoint Security Tools - signatures updated, modules checked (EDR sensor ON), 
+Intel Driver & Support Assistant updated – ()
+Backups - Egnyte
+
+
+need something to copy and paste documentation 
+need to edit registry to re enable auto updates 
+need something to launch then close cmd to run auto update
+how to start msstore from PS
+how to start settings from ps
+
+
+
 
 
 # https://github.com/oneget/oneget
 
 Install-Module -Name PackageManagement
+
+
