46224e67864a5b39ee7ffd96dab97e2cfbdb4b83
chickenj0 <121471036+chickenj0@users.noreply.github.com>
2024-02-15 13:33:30 +0800

    PROD - maintenance script

--- PsGalleryScript-HENRY2U.ps1
+++ PsGalleryScript-HENRY2U.ps1
@@ -1,67 +1,64 @@
-# Getting Started with the PowerShell Gallery
-# install powershell
+# PROD - Maintenance script
+
+Backups - Egnyte
+
+
+# Install PowerShell
 winget install --id Microsoft.Powershell --source winget
 
-Install PowerShell
 
+# Set Admin:Powershell as default terminal
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name 'PowerShell 7' -Value 'C:\Program Files\PowerShell\7\pwsh.exe'
 
-# set execution policy PS
+# Permissions set - RemoteSigned 
 Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned 
 
-Permissions set - RemoteSigned 
-
-
-# Create restore checkpoint
+# Enable Restore Point & Restore point 'MaintenanceRestorePoint' created
 Enable-ComputerRestore -Drive "C:\" && Checkpoint-Computer -Description "MaintenanceRestorePoint" -RestorePointType "MODIFY_SETTINGS"  
 
-Enable Restore Point & Restore point 'MaintenanceRestorePoint' created
+# Install PowerShellGet
+Install-Module PowerShellGet  
 
+# Install PSWindowsUpdate
+Install-Module PSWindowsUpdate
 
-# install powershellget
-Find-Module -Name PowerShellGet | Install-Module  
-Install-PackageProvider -Name NuGet
-
+# Wpdate WindowsOS & WinGet
 # PSWindowsUpdate update - ()
 # WinGet upgrade - ()
-Install-Module PSWindowsUpdate && install-module PowerShellGet
-
-
-# update WindowsOS & WinGet
-get-windowsupdate -install && winget upgrade --all
-
+Get-WindowsUpdate -Install && winget upgrade --all
 
 # Office & MS 365 updates - OfficeC2RClient.exe - checked
-
 cmd.exe
 whoami
 "C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe" /update <user>
 
+# TaskMgr telemetry - OK
+Taskmgr.exe
 
-
-# disk cleanup 
+# Disk cleanup - ( GB) 
 cleanmgr /sagerun:1 -Verbose 
 
-# disk defragment 
+# Disk ReTrim - allocations trimmed ()
 Optimize-Volume -DriveLetter C -Verbose
 Optimize-Volume -DriveLetter C -ReTrim -Verbose
 OR 
 Optimize-Volume -DriveLetter C DeFrag -Verbose
 
-# system file check 
+# System File Check – found corrupt files and successfully repaired / 
 sfc /scannow
 OR
 Start-Process -FilePath "C:\Windows\System32\sfc.exe" -ArgumentList 'verifyonly'
 Start-Process -FilePath "C:\Windows\System32\sfc.exe" -ArgumentList 'scannow'
 
-TaskMgr telemetry - OK
+
 Disk cleanup – () 
-Disk retrim - Allocations trimmed ()
-System file check – found corrupt files and successfully repaired 
+Disk retrim - 
+System file check 
 
 
 BitDefender Endpoint Security Tools - signatures updated, modules checked (EDR sensor ON), 
 Intel Driver & Support Assistant updated – ()
-Backups - Egnyte
+
 
 
 need something to copy and paste documentation 
