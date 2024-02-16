821fb5bf05aa467d565e17f5a317517bcae4b1b0
chickenj0 <121471036+chickenj0@users.noreply.github.com>
2024-02-16 11:46:01 +0800

    update

--- PROD -MaintenanceScript.ps1
+++ PROD -MaintenanceScript.ps1
@@ -1,52 +1,57 @@
 # PROD - Maintenance script
 
-# MSP360 Backup - N/A
+# MSP360 Backup - N/A , USB only , cloud only , USB then cloud , education given , schedule set
+    # Install windows caffeinated app
 # BitDefender Endpoint Security Tools - signatures updated, modules checked, EDR sensor ON, Device Control OFF
-# Intel Driver & Support Assistant updated – ()
+# Intel Driver & Support Assistant updated –
 
 # Install PowerShell
 winget install --id Microsoft.Powershell --source winget
 
-# Set Admin:Powershell as default terminal
+# Set Admin:Powershell to open on login
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name 'PowerShell 7' -Value 'C:\Program Files\PowerShell\7\pwsh.exe'
 
         # need logic here for if no terminal 
 
 # Permissions set - RemoteSigned 
-Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned 
-
-# Enable Restore Point & Restore point 'MaintenanceRestorePoint' created
-Enable-ComputerRestore -Drive "C:\" && Checkpoint-Computer -Description "MaintenanceRestorePoint" -RestorePointType "MODIFY_SETTINGS"  
-
+# Enable Restore Point & restore point 'MaintenanceRestorePoint' created / restore point already created
 # Install PowerShellGet
-Install-Module PowerShellGet  
-
 # Install PSWindowsUpdate
-Install-Module PSWindowsUpdate
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned ; Enable-ComputerRestore -Drive "C:\" && Checkpoint-Computer -Description "MaintenanceRestorePoint" -RestorePointType "MODIFY_SETTINGS"  ; Install-Module PowerShellGet  ; Install-Module PSWindowsUpdate
 
-# Update WindowsOS & WinGet
-# PSWindowsUpdate update - ()
-# WinGet upgrade - ()
-Get-WindowsUpdate -Install && winget upgrade --all
+# PSWindowsUpdate update -
+# WinGet update - 
+Get-WindowsUpdate -Install ; winget upgrade --all --include-unknown ; winget install microsoft.teams.classic
+
+# Adobe Acrobat Reader - checked
+# MS Store Apps - updated
+# Apple iTunes - checked
+# Microsoft Teams Classic - checked
+# VLC - checked
+# 7z - checked
+winget install adobe.acrobat.reader.64-bit ; winget install 9PBVHHSN78BL ; winget install apple.itunes ; winget install VideoLAN.VLC ; winget install 7zip.7zip
 
 # Office & MS 365 updates - OfficeC2RClient.exe - checked
 cmd.exe
 whoami
 "C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe" /update <user>
 
+# Restart
 # TaskMgr telemetry - OK
 Taskmgr.exe
 
-# Disk cleanup - ( GB) 
-cleanmgr /sagerun:1 -Verbose 
+# Disk cleanup - GB
+# Disk ReTrim - allocations trimmed 
+# System File Check – found corrupt files and successfully repaired
+cleanmgr ; Optimize-Volume -DriveLetter C -ReTrim -Verbose ; sfc /scannow
 
-# Disk ReTrim - allocations trimmed ()
+# Disk ReTrim - allocations trimmed 
 Optimize-Volume -DriveLetter C -Verbose
-Optimize-Volume -DriveLetter C -ReTrim -Verbose
+Optimize-Volume -DriveLetter C -ReTrim -Verbose ; sfc /scannow
 OR 
 Optimize-Volume -DriveLetter C DeFrag -Verbose
 
-# System File Check – found corrupt files and successfully repaired / 
+# System File Check – found corrupt files and successfully repaired /
 sfc /scannow
 OR
 Start-Process -FilePath "C:\Windows\System32\sfc.exe" -ArgumentList 'verifyonly'
@@ -61,6 +66,7 @@ Start-Process -FilePath "C:\Windows\System32\sfc.exe" -ArgumentList 'scannow'
 # need something to copy and paste documentation 
 # need to edit registry to re enable auto updates 
 # need something to launch then close cmd to run auto update
+    # check windows install scripts??
 # how to start msstore from PS
 # how to start settings from ps
 
