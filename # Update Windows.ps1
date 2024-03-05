4bc2ccbaa1e7c0baf40cb481416acdecc45f7039
chickenj0 <zyxwv@duck.com>
2024-03-05 16:18:33 +0800

    maintenance 0.0.3

--- # Update Windows.ps1
+++ # Update Windows.ps1
@@ -1,39 +1,44 @@
 # Setting Permissions
-Write-host "Setting permissions to RemoteSigned..."
-set-executionpolicy -scope currentuser -executionpolicy remotesigned
+Write-host "Setting permissions to RemoteSigned..." ; set-executionpolicy -scope currentuser -executionpolicy remotesigned
+
+# Creating restore point 
+Write-host "Creating restore point..." ; Checkpoint-Computer -Description "MaintenanceRestorePoint" -RestorePointType "MODIFY_SETTINGS"
 
 # Update Windows
-Write-Host "Checking for Windows updates..."
-Install-PackageProvider -Name NuGet -Force -Scope CurrentUser
-Install-Module PSWindowsUpdate -Force -AllowClobber -Scope CurrentUser
-Import-Module PSWindowsUpdate
-Get-WindowsUpdate -Install -AcceptAll 
+Write-Host "Checking for Windows updates..." ; Install-PackageProvider -Name NuGet -Force -Scope CurrentUser ; Install-Module PSWindowsUpdate -Force -AllowClobber -Scope CurrentUser ; Import-Module PSWindowsUpdate ; Get-WindowsUpdate -Install -AcceptAll 
+        # Set-PSRepository cmdlet
+
 
 # Update PowerShellGet
-Write-Host "Updating PowerShellGet..."
-Install-Module PowerShellGet -Force -AllowClobber -Scope CurrentUser
+Write-Host "Updating PowerShellGet..." ; Install-Module PowerShellGet -Force -AllowClobber -Scope CurrentUser
 
 # Update all PowerShell modules
-Write-Host "Updating all PowerShell modules..."
-Update-Module -Name *
+Write-Host "Updating all PowerShell modules..." ; Update-Module -Name *
 
 # Updating WinGet package manager
-Write-Host "Updating WinGet package manager..."
-Winget upgrade --all --include-unknown
+Write-Host "Updating WinGet package manager..." ; Winget upgrade --all --include-unknown
+
 
 # Start MS Windows Update
-Write-Host "Opening Windows Update..."
-Start-Process "ms-settings:windowsupdate"
+Write-Host "Opening Windows Update..." ; Start-Process "ms-settings:windowsupdate" ; Write-Host "Starting MS Windows Store Downloads and Updates..." ; Start-Process "ms-windows-store://downloadsandupdates"
 
-# Openning MS Store Downloads and Updates
+
+
+# Opening MS Store Downloads and Updates
 # RTFM: https://learn.microsoft.com/en-us/windows/uwp/launch-resume/launch-store-app
-Write-Host "Starting MS Windows Store Downloads and Updates..."
-Start-Process "ms-windows-store://downloadsandupdates"
+
+
+# Retrieving Intel Driver Updates
+Write-Host "Retrieving Intel Driver Updates"
+Start-Process ""C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Intel Driver & Support Assistant.lnk""
+
 
 # Open storage sense
 Start-Process "ms-settings:storagesense"
 
-Chocolatey.Chocolatey
+
+
+# Chocolatey.Chocolatey
 
 # Check for driver updates (This is a generic example, might not cover all devices)
 Write-Host "Checking for driver updates..."
